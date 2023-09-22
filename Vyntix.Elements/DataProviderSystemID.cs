b5c367f636f61912fd675e32ed11078e719217be
Sam <sam.wheat@outlook.com>
2023-09-22 08:01:24 -0700

    Add DataProviderSystemID

---
+++ Vyntix.Elements/DataProviderSystemID.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaderAnalytics.Vyntix.Elements;
+
+public enum DataProviderSystemID
+{
+    UserDefined = 0,
+    FRED = 1,
+    Quandl = 2
+}
---
+++ Vyntix.Elements/IDataProviderConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace LeaderAnalytics.Vyntix.Elements;
+
+public interface IDataProviderConfig
+{
+    int ID { get; }
+    string Name { get; }
+    int SystemID { get; }
+    string DownloadURI { get; }
+    string Login { get; }
+    string Password { get; }
+    DateTime? LastDownload { get; }
+    DownloadResult LastDownloadResult { get; }
+    int UserID { get; }
+}
