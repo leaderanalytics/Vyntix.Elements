5a8120e476c17dfe80cd99f4381711acc62bd621
Sam <sam.wheat@outlook.com>
2023-09-24 11:52:37 -0700

    Fix IDataProvider

--- Vyntix.Elements/IDataProvider.cs
+++ Vyntix.Elements/IDataProvider.cs
@@ -7,7 +7,6 @@ public interface IDataProvider
     int SystemID { get; set; }
     string Setting1 { get; set; }
     string Setting2 { get; set; }
-    string Password { get; set; }
     DateTime? LastDownloadDate { get; set; }
     DownloadResult LastDownloadResult { get; set; }
     int UserID { get; set; }
