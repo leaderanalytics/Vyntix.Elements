b8c10d2ce9a7363594cba90c20bbc96046c8b0f5
Sam <sam.wheat@outlook.com>
2023-09-24 10:06:41 -0700

    Add IDataProvider

---
+++ Vyntix.Elements/IDataProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace LeaderAnalytics.Vyntix.Elements;
+
+public interface IDataProvider
+{
+    int ID { get; set; }
+    string Name { get; set; }
+    int SystemID { get; set; }
+    string DownloadURI { get; set; }
+    string Login { get; set; }
+    string Password { get; set; }
+    DateTime? LastDownload { get; set; }
+    DownloadResult LastDownloadResult { get; set; }
+    int UserID { get; set; }
+}
