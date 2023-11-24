5290de5c820a72b4753daac0c7cbca3372fbb76a
Sam <sam.wheat@outlook.com>
2023-11-24 11:47:55 -0800

    Add APIResult

---
+++ Vyntix.Elements/APIResult.cs
@@ -0,0 +1,20 @@
+﻿namespace LeaderAnalytics.Vyntix.Elements;
+
+public class APIResult
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+}
+
+public class APIResult<T> : APIResult
+{
+    public T Data { get; set; }
+
+    public APIResult() { }
+
+    public APIResult(T data, bool success = true)
+    {
+        this.Success = success;
+        this.Data = data;
+    }
+}
